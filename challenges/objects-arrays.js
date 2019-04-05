2193f1440a3f31742946e8e90b40afcb330d16c9
droneal11015 <droneal11015@gmail.com>
2019-04-05 11:29:56 -0400

    created console logs for dinos, added 'roar' method for trex

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -18,6 +18,9 @@ const trex = dinosaur ({
   weight: '7000kg',
   length: '12m',
   period: 'Late Cretaceious'
+  roar: function(){
+    return 'RAWERSRARARWERSARARARRRR!'
+  }
 });
 
 const stego = dinosaur ({
@@ -39,20 +42,20 @@ const raptor = dinosaur ({
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(trex.weight());
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(raptor.diet());
 
 // How long was a stegosaurus?
-console.log();
+console.log(stego.length());
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(trex.period());
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(trex.roar());
 
 
 // ==== Arrays ====
